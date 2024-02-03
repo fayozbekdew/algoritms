c6d5990cd855afa137dd096c9b676eeae0a4bd13
fayozbekdew <fayozbekdev@gmail.com>
2024-02-03 18:39:51 +0500

    add

--- leetCode.js
+++ leetCode.js
@@ -75,3 +75,21 @@ var fibGenerator = function*() {
 //   console.log(aa.next().value);
 //   console.log(aa.next().value);
   
+
+// Given an array arr and a chunk size size, return a chunked array. A chunked array contains the original elements in arr, but consists of subarrays each of length size. The length of the last subarray may be less than size if arr.length is not evenly divisible by size.
+
+// You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
+
+// Please solve it without using lodash's _.chunk function.
+var chunk = function(arr, size) {
+    var arrCh = [];
+    var index = 0;
+    while (index < arr.length) {
+      arrCh.push(arr.slice(index, index + size));
+      index += size;
+      // 0 + 3  index = 3
+      // 3 + 3  index = 6
+      // condition failed while does not work  
+    }
+    return arrCh;
+}
