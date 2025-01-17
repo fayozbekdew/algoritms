e0351b2c231e624c343b94f203b72f96f67bf3de
Fayozbek Borotboyev <fayozbekdev@gmail.com>
2025-01-17 11:14:27 +0500

    day 5  solution

--- leetcode/day-5/index.js
+++ leetcode/day-5/index.js
@@ -1,4 +1,4 @@
-//! question number:153 == topic:array,binary search == level:medium
+//! question number:153,154 == topic:array,binary search == level:medium,hard
 //? Savol: Sizga aynaladigan ro'yhat beriladi.Uni ichida eng kichik elementini O(log n) time da toping
 // Misol [4,5,1,2,3] bu 2 marta aylangan ro'yhat chunki asl korinish [1.2.3.4.5] bo'lgan
 
@@ -23,3 +23,53 @@ var findMin = function(nums) {
     }
     return nums[start]
 };
+
+
+
+//Question number: 155 == topic:Stack,design == level:medium
+// Stack uchun arxitektura yozish kerak
+
+var MinStack = function() {
+    this.stack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push({
+        value: val,
+        minVal: this.stack.length == 0 ? val : Math.min(val,this.getMin())
+    })
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1].value
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.stack[this.stack.length - 1].minVal;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
